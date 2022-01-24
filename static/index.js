aa54f3f6adf3b19b477a32bee8d8985e861e8c6c
sekiro234 <98271232+sekiro234@users.noreply.github.com>
2022-01-24 16:08:39 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,9 +341,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
+    if (score <= 19) return '下不去手？你真的不是汉奸吗？';
+    if (score <= 39) return '抗日新手';
+    if (score <= 59) return '天皇杀手蟹';
+    if (score <= 99) return '手撕鬼子，抗日英雄';	
+    if (score <= 149) return '屠天皇者，终成天皇';
     if (score <= 199) return '您';
     return '人？';
 }
